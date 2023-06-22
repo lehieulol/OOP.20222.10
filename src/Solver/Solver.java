d2c4c069b8af761133c945bc976a62f51ca7c51d
hwng and hwng <91714479+hwnginsoict@users.noreply.github.com>
2023-06-22 11:21:18 +0700

    Update Solver.java

--- src/Solver/Solver.java
+++ src/Solver/Solver.java
@@ -1,6 +1,7 @@
 package Solver;
 
 import javax.swing.JPanel;
+import algorithm.QuineMcCluskey;
 
 public class Solver {
 	/**
@@ -11,7 +12,17 @@ public class Solver {
 	 * @return 0 is written as 'A' and so on; if returned number >= num_of_variable, it is written as negative of its modulo of num_of_variable
 	 */
 	public static int[][] solve(Integer num_of_variable, int[] truth_table, String output_type, JPanel process_output){
-		int[][] ret = {{2},{4,2},{0,1,5},{2,3,4,5}};
+		int t[] = {0,4,5,7,8,11,12,15};
+		truth_table = t;
+		QuineMcCluskey start = new QuineMcCluskey();
+		int[][] ret = start.result(truth_table, num_of_variable);
+//		System.out.println(truth_table.length);
+		for(int i=0; i<ret.length; i++) {
+			for(int j=0; j<ret[i].length; j++) {
+				System.out.print(ret[i][j] + " ");
+			}
+			System.out.println();
+		}
 		return ret;
 	}
 }
