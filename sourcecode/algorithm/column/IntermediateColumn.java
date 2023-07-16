fcb4a1a18877ab7a6595988db1fa69f9d182d6b5
hwng and hwng <91714479+hwnginsoict@users.noreply.github.com>
2023-07-16 12:27:08 +0700

    add comment, remove submain

--- sourcecode/algorithm/column/IntermediateColumn.java
+++ sourcecode/algorithm/column/IntermediateColumn.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import algorithm.MinTerm;
 public class IntermediateColumn extends Column {
 	
-	private ArrayList<ArrayList<Integer>> primeImplicants = new ArrayList<ArrayList<Integer>>();
-	private ArrayList<ArrayList<Integer>> checkList = new ArrayList<ArrayList<Integer>>();
+	private ArrayList<ArrayList<Integer>> primeImplicants = new ArrayList<ArrayList<Integer>>(); // to save prime implicants
+	private ArrayList<ArrayList<Integer>> checkList = new ArrayList<ArrayList<Integer>>(); // to check prime implicants
 	
 	public IntermediateColumn(ArrayList<ArrayList<ArrayList<Integer>>> column, int lengthOfBits){
 		this.setLengthOfBits(lengthOfBits);
@@ -18,7 +18,7 @@ public class IntermediateColumn extends Column {
 	public ArrayList<ArrayList<ArrayList<Integer>>> step(ArrayList<ArrayList<ArrayList<Integer>>> list) {
 		ArrayList<ArrayList<ArrayList<Integer>>> result = new ArrayList<ArrayList<ArrayList<Integer>>>();
 		for(int i = 0; i < list.size()-1; i++) {
-			result.add(new ArrayList<ArrayList<Integer>>()); //tao number of index 0 = prestep -1
+			result.add(new ArrayList<ArrayList<Integer>>()); //create number of index 0 = prestep -1
 		}
 		
 		ArrayList<Integer> temp;
@@ -29,20 +29,20 @@ public class IntermediateColumn extends Column {
 					if(temp.size()!=0) {
 						result.get(i).add(temp);
 						this.checkList.get(i).set(j, 1);
-						this.checkList.get(i+1).set(k,1);
+						this.checkList.get(i+1).set(k,1); //if can compare, set it to 1 to go to next step
 					}
 				}
 			}
 		}
 		for(int i=0; i < checkList.size(); i++) {
 			for(int j=0; j<checkList.get(i).size();j++) {
-				if(checkList.get(i).get(j)==0) {
+				if(checkList.get(i).get(j)==0) { //checklist remain 0
 					temp = new ArrayList<Integer>();
 					for(int k= this.lengthOfBits; k<list.get(i).get(j).size(); k++) {
 						if(temp.contains(list.get(i).get(j).get(k))) {
 							
 						}
-						else temp.add(list.get(i).get(j).get(k));
+						else temp.add(list.get(i).get(j).get(k)); //if remains 0, so it is prime implicant
 					}
 					this.primeImplicants.add(temp);
 				}
@@ -53,7 +53,7 @@ public class IntermediateColumn extends Column {
 	}
 	
 	
-	public void generateCheckList(ArrayList<ArrayList<ArrayList<Integer>>> list) {
+	public void generateCheckList(ArrayList<ArrayList<ArrayList<Integer>>> list) { //create checklist, set all to 0
 		for(int i=0; i<list.size(); i++) {
 			this.checkList.add(new ArrayList<Integer>());
 			for(int j=0; j<list.get(i).size(); j++) {
@@ -62,7 +62,7 @@ public class IntermediateColumn extends Column {
 		}
 	}
 	
-	public ArrayList<Integer> compare(ArrayList<Integer> a1, ArrayList<Integer> a2){
+	public ArrayList<Integer> compare(ArrayList<Integer> a1, ArrayList<Integer> a2){ //compare 2 binary represent, if different in only 1 positions, can return
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		for(Integer i: a1) {
 			result.add(i);
@@ -95,7 +95,7 @@ public class IntermediateColumn extends Column {
 		}
 		return count;
 	}
-	public int noOfNines(ArrayList<Integer> num) {
+	public int noOfNines(ArrayList<Integer> num) { //number of dont care values in binary representation
 		int count = 0;
 		for (int i=0; i<lengthOfBits; i++) {
 			if (num.get(i) == 9) {
@@ -110,30 +110,6 @@ public class IntermediateColumn extends Column {
 	}
 	
 	
-	public static void main(String[] args){
-		ArrayList<Integer> b = new ArrayList<Integer>();
-		b.add(0);
-		b.add(2);
-		b.add(4);
-		b.add(5);
-		b.add(8);
-		b.add(7);
-		b.add(11);
-		b.add(12);
-		b.add(15);
-		ArrayList<MinTerm> c = new ArrayList<MinTerm>();
-		for(Integer k: b) {
-			int i = k;
-			c.add(new MinTerm(i,4));
-		}
-		FirstColumn a = new FirstColumn(c,4);
-		IntermediateColumn a1 = new IntermediateColumn(a.getColumn(),4);
-		IntermediateColumn a2 = new IntermediateColumn(a1.getColumn(),4);
-		IntermediateColumn a3 = new IntermediateColumn(a2.getColumn(),4);
-		System.out.println(a1.getPrimeImplicants());
-		System.out.println(a2.getPrimeImplicants());
-		System.out.println(a3.getPrimeImplicants());
-	}
 	
 	
 	
