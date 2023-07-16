24702a255e2391c4cb56a19489c2a89efdedbc63
hwng and hwng <91714479+hwnginsoict@users.noreply.github.com>
2023-07-16 12:19:17 +0700

    add comments

--- sourcecode/algorithm/TransformColumn.java
+++ sourcecode/algorithm/TransformColumn.java
@@ -7,7 +7,7 @@ import algorithm.column.IntermediateColumn;
 
 public class TransformColumn {
 	
-	private ArrayList<ArrayList<Integer>> collectPrimeImplicants = new ArrayList<ArrayList<Integer>>();
+	private ArrayList<ArrayList<Integer>> collectPrimeImplicants = new ArrayList<ArrayList<Integer>>(); //to save all prime implicants
 	private int lengthOfBits;
 	private int noOfMinTerms;
 	private ArrayList<Integer> minTerms;
@@ -18,11 +18,11 @@ public class TransformColumn {
 		this.setMinTerms(minTerms);
 		ArrayList<ArrayList<Integer>> temp = new ArrayList<ArrayList<Integer>>();
 		
-//		tranh vong while dau
+//		avoid first while
 		temp.add(new ArrayList<Integer>());
 //		ArrayList<Integer> to;
-		IntermediateColumn interColumn0 = new IntermediateColumn(firstColumn.getColumn(), this.getLengthOfBits());
-		IntermediateColumn interColumn = new IntermediateColumn(firstColumn.getColumn(), this.getLengthOfBits());
+		IntermediateColumn interColumn0 = new IntermediateColumn(firstColumn.getColumn(), this.getLengthOfBits()); //column to save the prestep
+		IntermediateColumn interColumn = new IntermediateColumn(firstColumn.getColumn(), this.getLengthOfBits()); //column to save the intermediate step
 		int i = 0;
 		while(i<6) {
 //			temp = new ArrayList<ArrayList<Integer>>();
@@ -40,8 +40,8 @@ public class TransformColumn {
 				else this.collectPrimeImplicants.add(k);
 			}
 //			System.out.println(1);
-			interColumn = new IntermediateColumn(interColumn0.getColumn(), this.getLengthOfBits());
-			interColumn0 = new IntermediateColumn(interColumn0.getColumn(), this.getLengthOfBits());
+			interColumn = new IntermediateColumn(interColumn0.getColumn(), this.getLengthOfBits()); // intermediate step = next of prestep
+			interColumn0 = new IntermediateColumn(interColumn0.getColumn(), this.getLengthOfBits()); // prestep moves on
 			i++;
 		}
 		
@@ -50,12 +50,12 @@ public class TransformColumn {
 			k.add(array);
 		}
 			
-		this.collectPrimeImplicants = this.simplifyImplicants(k);
+		this.collectPrimeImplicants = this.simplifyImplicants(k); // simplify prime implicants
 		
 	}
 	
 	
-	public ArrayList<ArrayList<Integer>> simplifyImplicants(ArrayList<ArrayList<Integer>> implicants){
+	public ArrayList<ArrayList<Integer>> simplifyImplicants(ArrayList<ArrayList<Integer>> implicants){ //simplify implicants to shorten the expression
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		ArrayList<ArrayList<Integer>> noOfAppear = new ArrayList<ArrayList<Integer>>();
 		int count;
@@ -72,7 +72,7 @@ public class TransformColumn {
 			temp.add(count);
 			noOfAppear.add(temp);
 		}
-		Collections.sort(noOfAppear, (a, b)->a.get(1).compareTo(b.get(1)));
+		Collections.sort(noOfAppear, (a, b)->a.get(1).compareTo(b.get(1))); //group of implicants with more minterms is prioritized
 		
 		ArrayList<Integer> s0 = new ArrayList<Integer>(); 
 		ArrayList<Integer> sc = new ArrayList<Integer>(); 
